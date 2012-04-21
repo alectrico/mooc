7cc85c250ca15754062a3a095783e3f445893755
Jeremy Tennant <registrations@jeremytennant.com>
2012-04-22 00:54:48 +1000

    Finished user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Jeremy Tennant"
+    email    "registrations@jeremytennant.com"
+    password "temp00"
+    password_confirmation "temp00"
+  end
+end
