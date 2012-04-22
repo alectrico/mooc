761c77fa3a5935c66fe3f53368087130789eac32
Jeremy Tennant <registrations@jeremytennant.com>
2012-04-22 23:19:43 +1000

    Added valid and invalid signup tests

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+
+	def new
+		#code here		
+	end
+
+	def create
+		user = User.find_by_email(params[:session][:email])
+		if user && user.authenticate(params[:session][:password])
+			sign_in user
+  			redirect_to user
+		else
+			flash.now[:error] = 'Invalid email/password combination'
+			render 'new'
+		end
+	end
+
+	def destroy
+		#code here
+	end
+
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-		flash[:success] = "Welcome to the Sample App!"
-		redirect_to @user
+  		flash[:success] = "Welcome to the Sample App!"
+  		redirect_to @user
     else
       render 'new'
     end
@@ -16,6 +16,6 @@ class UsersController < ApplicationController
 
 	def show
   	  @user = User.find(params[:id])
-  	end
+  end
   	
 end
